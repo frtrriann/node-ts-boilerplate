5d397cc7a8a52c26b5be94a081163d6dc07c8e59
frtrriann <frtrriann@outlook.com>
2023-04-23 22:22:03 +0300

    update packages, add examples with fastify, jest, and dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
 import "dotenv/config";
+import Fastify from 'fastify'
+
+const fastify = Fastify({
+  logger: true
+})
+
+const port = Number(process.env.PORT) || 0;
+
+fastify.get('/', (request, reply) => {
+  reply.send({ hello: 'world' })
+})
+
+// Run the server!
+fastify.listen({ port }, (err, address) => {
+  if (err) throw err
+  console.warn(`Server is now listening on ${address}`)
+})
